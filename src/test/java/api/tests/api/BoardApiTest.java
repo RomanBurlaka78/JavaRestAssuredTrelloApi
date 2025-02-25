9874914c4da22e6c5974ab0de099f489fd6a1581
Roman Burlaka <burlakarv@gmail.com>
2025-02-25 23:19:11 +0100

    influxdb, grafana

--- src/test/java/api/tests/api/BoardApiTest.java
+++ src/test/java/api/tests/api/BoardApiTest.java
@@ -12,6 +12,11 @@ import org.checkerframework.checker.units.qual.A;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 
@@ -23,7 +28,26 @@ public class BoardApiTest extends BaseTest {
 
     private BoardSteps boardSteps = new BoardSteps();
 
+    @DataProvider
+    public Object[][] fieldData() {
+        return new Object[][]{
+                {"closed"},
+                {"dateLastActivity"},
+                {"dateLastView"},
+                {"descData"},
+                {"name"},
+        };
+    }
 
+    @DataProvider
+    public Object[][] filteredCards() {
+        return new Object[][]{
+                {"all"},
+                {"closed"},
+                {"open"},
+                {"visible"},
+        };
+    }
 
 
     @Test(priority = 1, description = "Create Board Validation")
@@ -32,11 +56,19 @@ public class BoardApiTest extends BaseTest {
     @Severity(SeverityLevel.CRITICAL)
     public void testCreateBoard() {
         Response response = boardSteps.createBoard("Api Board");
-        TestData.boardId =  response.path("id").toString();
+        TestData.boardId = response.path("id").toString();
 
         Assert.assertTrue(!response.jsonPath().getString("id").isEmpty());
         Assert.assertEquals(response.getStatusCode(), 200);
+    }
 
+    @Test(priority = 2, description = "Create Board Validation")
+    @Story("Verify created label")
+    @Description("Get labels on board")
+    @Severity(SeverityLevel.NORMAL)
+    public void testGetLabelOnBoard() {
+        Response response = boardSteps.getLabelsOnBoard();
+        Assert.assertEquals(response.getStatusCode(), 200);
     }
 
 
@@ -48,5 +80,66 @@ public class BoardApiTest extends BaseTest {
         Response response = boardSteps.deleteBoardStep(TestData.boardId);
         Assert.assertEquals(response.getStatusCode(), 200);
     }
+
+    @Ignore
+    @Test(priority = 3)
+    @Story("Verify update board")
+    @Description("Update board")
+    @Severity(SeverityLevel.NORMAL)
+    public void testUpdateBoard() {
+        Response response = boardSteps.updateBoard("Updated board");
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+
+    @Test(priority = 3, dataProvider = "fieldData")
+    @Story("Verify  board")
+    @Description("Get field on a board")
+    @Severity(SeverityLevel.NORMAL)
+    public void testGetField(String fieldData) {
+        Response response = boardSteps.getField(fieldData);
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+    @Test(priority = 2, dataProvider = "filteredCards", dependsOnMethods = "testCreateBoard")
+    @Story("Verify  board")
+    @Description("Get filtered cards on a board")
+    @Severity(SeverityLevel.NORMAL)
+    public void testGetFilteredCards(String filteredData) {
+        Response response = boardSteps.getFilteredCards(filteredData);
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+    @Ignore
+    @Test(priority = 3, dependsOnMethods = "testCreateBoard")
+    @Story("Verify  board")
+    @Description("Create label on a board")
+    @Severity(SeverityLevel.NORMAL)
+    public void testCreateLabel() {
+        Response response = boardSteps.createLabel("Backlog");
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+    @Test(priority = 4, dependsOnMethods = "testCreateBoard")
+    @Story("Verify  board")
+    @Description("Create label on a board")
+    @Severity(SeverityLevel.NORMAL)
+    public void testGetListsOnBoard() {
+        Response response = boardSteps.getListsOnBoard();
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+    @Test(priority = 3, dependsOnMethods = "testCreateBoard")
+    @Story("Verify  board")
+    @Description("Create list on a board")
+    @Severity(SeverityLevel.NORMAL)
+    public void testCreateListsOnBoard() {
+        Response response = boardSteps.createListOnBoard("Backlog");
+        TestData.idList = response.path("id");
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
 
 }
