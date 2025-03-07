4894c92813bd0f9977af4b897d20141729658f2c
KorobovS <korobovs33@yandex.ru>
2025-03-08 00:19:01 +0300

    add response specification

--- src/test/java/api/utils/ApiClient.java
+++ src/test/java/api/utils/ApiClient.java
@@ -9,57 +9,64 @@ import org.apache.logging.log4j.Logger;
 public class ApiClient {
 
     private static ApiClient instance;
-    private RequestSpecification requestSpec;
+    private static RequestSpecification requestSpec;
+    private static Specification specification = new Specification();
     private static final Logger logger = LogManager.getLogger(ApiClient.class);
-    Specification specification= new Specification();
 
     private ApiClient() {
         logger.info("Creating ApiClient instance");
     }
 
     public static ApiClient getInstance() {
+
         if (instance == null) {
             logger.info("Initializing new ApiClient instance");
             instance = new ApiClient();
         }
+
         return instance;
     }
 
-    public RequestSpecification getRequestSpec() {
+    public void getRequestSpec() {
+
         requestSpec = RestAssured.given()
-                // Используем RequestSpecification, установленный в BaseTest
                 .spec(specification.installRequest())
                 .contentType("application/json");
-
-        return requestSpec;
     }
 
     public Response get(String path) {
         logger.info("Executing GET request to: " + path);
+
         return requestSpec.get(path);
     }
 
     public Response post(String path, String body) {
         logger.info("Executing POST request to: " + path);
+
         return requestSpec.body(body).post(path);
     }
+
     public Response postWithOutBody(String path, String name) {
         logger.info("Executing POST request to: " + path);
+
         return requestSpec.queryParam("name", name).post(path);
     }
 
     public Response put(String path, String body) {
         logger.info("Executing PUT request to: " + path);
+
         return requestSpec.body(body).put(path);
     }
 
     public Response patch(String path, String body) {
         logger.info("Executing PUT request to: " + path);
+
         return requestSpec.body(body).patch(path);
     }
 
     public Response delete(String path) {
         logger.info("Executing DELETE request to: " + path);
+
         return requestSpec.delete(path);
     }
 }
