e2615c141076a2655da132e146386d994e6c2be5
Dmitri Rysak <dimarusak5@gmail.com>
2025-04-10 12:59:42 +0300

    create MembersAPITest and test testGetAMember()

---
+++ src/test/java/api/steps/MembersSteps.java
@@ -0,0 +1,15 @@
+package api.steps;
+
+import api.base.PathParameters;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+
+public class MembersSteps extends BaseService{
+
+    @Step("Get a member with id - {'firstMemberId'}")
+    public Response getAMember(String firstMemberId) {
+        Response response = apiClient.get(PathParameters.MEMBERS_BASE_PATH + firstMemberId, requestSpecification);
+        initRequestSpecification();
+        return response;
+    }
+}
