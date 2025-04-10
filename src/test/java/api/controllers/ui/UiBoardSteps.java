fc336db6c0d12bf123c74be9a0041f793a2818db
Roman Burlaka <burlakarv@gmail.com>
2025-04-10 22:10:34 +0200

    updated locators and structure

--- src/test/java/api/controllers/ui/UiBoardSteps.java
+++ src/test/java/api/controllers/ui/UiBoardSteps.java
@@ -21,13 +21,15 @@ public class UiBoardSteps extends BaseUiTest {
         login();
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        WebElement board = getDriver().findElement(By.xpath("//div[@data-testid='TeamTabSection']//parent::ul/li[3]/a"));
-        board.click();
+//        WebElement board = getDriver().findElement(By.xpath("//div[@data-testid='TeamTabSection']//parent::ul/li[3]/a"));
+//        WebElement board = getDriver().findElement
+//                (By.xpath("//span[contains(text(), 'TrelloApiForward')]"));
+//        board.click();
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        WebElement boardApi = getDriver().findElement(By.xpath("//div[@title='Api Board']/div"));
+//        WebElement boardApi = getDriver().findElement(By.xpath("//div[@title='Api Board']/div"));
 
 
-        Assert.assertEquals(boardApi.getText(), "Api Board");
+        Assert.assertEquals(getDriver().getCurrentUrl(), "https://trello.com/u/romanbv/boards");
     }
 
     @Step("Close browser Trello UI and delete driver")
@@ -42,17 +44,18 @@ public class UiBoardSteps extends BaseUiTest {
     public void getBoardNameUI() {
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         WebElement board = getDriver().findElement(By.xpath("//*[contains(text(),'Api Board')]"));
-        Assert.assertEquals(board.getText(),"Api Board");
-        takeScreenshot(getDriver(),"testGetBoard", "BoardApiTest");
+        Assert.assertEquals(board.getText(), "Api Board");
+        takeScreenshot(getDriver(), "testGetBoard", "BoardApiTest");
         Allure.addAttachment("Trello UI board Screenshot", "image/png",
-                Arrays.toString(((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.BYTES)));
+                Arrays.toString(((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES)));
 
     }
+
     @Step("Verify updated board name in Trello UI")
     public void getUINewBoardName() {
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         WebElement board = getDriver().findElement(By.xpath("//*[contains(text(),'New Api Board')]"));
-        Assert.assertEquals(board.getText(),"New Api Board");
+        Assert.assertEquals(board.getText(), "New Api Board");
 
 
     }
