6152c259488b169da9e0667437a2fc92370ca0ee
Roman Burlaka <burlakarv@gmail.com>
2025-04-10 14:00:53 +0200

    updated CI

--- src/test/java/api/controllers/ui/UiBoardSteps.java
+++ src/test/java/api/controllers/ui/UiBoardSteps.java
@@ -4,6 +4,7 @@ import api.base.BaseUiTest;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class UiBoardSteps extends BaseUiTest {
     @Step("Verify board exists in Trello UI")
     public void verifyLoginInUI() throws IOException {
         login();
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         WebElement board = getDriver().findElement(By.xpath("//*[contains(text(),'Api Board')]"));
 
         Assert.assertTrue(board.isDisplayed(), "Created board is not displayed in UI");
@@ -21,6 +23,7 @@ public class UiBoardSteps extends BaseUiTest {
 
     @Step("Close browser Trello UI and delete driver")
     public void closeBrowserAndDriver() {
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         logOut();
 
         Assert.assertEquals(getDriver(), null);
@@ -28,6 +31,7 @@ public class UiBoardSteps extends BaseUiTest {
 
     @Step("Verify board name in Trello UI")
     public void getBoardNameUI() {
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         WebElement board = getDriver().findElement(By.xpath("//*[contains(text(),'Api Board')]"));
         Assert.assertEquals(board.getText(),"Api Board");
         takeScreenshot(getDriver(),"testGetBoard", "BoardApiTest");
